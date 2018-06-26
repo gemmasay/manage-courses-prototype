1bda26192af836c74fdcdb93f226318e387003a8
Paul Hayes <fofr@users.noreply.github.com>
2018-06-26 09:23:47 +0100

    Birmingham City University test

--- generate.rb
+++ generate.rb
@@ -5,7 +5,7 @@ require 'json'
 
 file = File.read('courses-clean.json')
 data = JSON.parse(file)
-provider = 'Cornwall School Centred Initial Teacher Training (Cornwall SCITT)'
+provider = 'Birmingham City University'
 courses = data.select {|c| c['provider'] == provider }
 
 prototype_data = {
