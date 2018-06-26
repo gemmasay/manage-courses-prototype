f79332378473a1fbfb23fa231a5f5165da4a3d77
Paul Hayes <fofr@users.noreply.github.com>
2018-06-26 09:23:06 +0100

    Cornwall SCITT test

--- generate.rb
+++ generate.rb
@@ -5,7 +5,7 @@ require 'json'
 
 file = File.read('courses-clean.json')
 data = JSON.parse(file)
-provider = 'Dixons Teacher Training'
+provider = 'Cornwall School Centred Initial Teacher Training (Cornwall SCITT)'
 courses = data.select {|c| c['provider'] == provider }
 
 prototype_data = {
