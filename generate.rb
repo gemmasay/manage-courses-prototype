88e2c2bf9cfa87232ae988fa499364ab8001c1c5
Paul Hayes <fofr@users.noreply.github.com>
2018-06-27 11:22:55 +0100

    St Mary's test

--- generate.rb
+++ generate.rb
@@ -5,7 +5,7 @@ require 'json'
 
 file = File.read('courses-clean.json')
 data = JSON.parse(file)
-provider = 'Gorse SCITT'
+provider = 'St Mary\'s University, Twickenham'
 courses = data.select {|c| c['provider'] == provider }
 
 prototype_data = {
