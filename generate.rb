ee217d259bbd2800f1d3a567f2511b82334472f3
Paul Hayes <fofr@users.noreply.github.com>
2018-07-02 11:49:00 +0100

    Create multi-org design
    * Start page listing organisations and number of courses for each (some orgs may have 0?)
    * Updated breadcrumb to use "Organisations" as root
    * Removed top-level nav items as they are not per-org (and not really needed yet)
    
    https://dfedigital.atlassian.net/browse/BATSA-401

--- generate.rb
+++ generate.rb
@@ -5,7 +5,7 @@ require 'json'
 
 file = File.read('courses-clean.json')
 data = JSON.parse(file)
-provider = 'Catholic Teaching Alliance (South East London)'
+provider = 'Astra SCITT'
 courses = data.select {|c| c['provider'] == provider }
 
 prototype_data = {
