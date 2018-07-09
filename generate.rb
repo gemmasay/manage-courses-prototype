9d505216e3066631aa7dcf57f791973fce80928b
Paul Hayes <fofr@users.noreply.github.com>
2018-07-09 15:22:32 +0100

    Prep Bromley org Darrick

--- generate.rb
+++ generate.rb
@@ -5,7 +5,7 @@ require 'json'
 
 file = File.read('courses-clean.json')
 data = JSON.parse(file)
-provider = 'Warren Road Primary'
+provider = 'Darrick Wood School'
 courses = data.select {|c| c['provider'] == provider }
 
 prototype_data = {
