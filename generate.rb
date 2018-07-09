1e15a96003cae251540ff19357fc35de254a2885
Paul Hayes <fofr@users.noreply.github.com>
2018-07-09 15:20:40 +0100

    Prep Bromley org Lewisham

--- generate.rb
+++ generate.rb
@@ -5,7 +5,7 @@ require 'json'
 
 file = File.read('courses-clean.json')
 data = JSON.parse(file)
-provider = 'Bromley Schools Collegiate'
+provider = 'Education Teaching Alliance Lewisham'
 courses = data.select {|c| c['provider'] == provider }
 
 prototype_data = {
