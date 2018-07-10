0b825b433e64eceb15c98be85db2ec22b622ba0b
Paul Hayes <fofr@users.noreply.github.com>
2018-07-10 09:42:24 +0100

    Prep for UWE test

--- generate.rb
+++ generate.rb
@@ -5,7 +5,7 @@ require 'json'
 
 file = File.read('courses-clean.json')
 data = JSON.parse(file)
-provider = 'University of Sunderland'
+provider = 'University of the West of England, Bristol'
 courses = data.select {|c| c['provider'] == provider }
 
 prototype_data = {
