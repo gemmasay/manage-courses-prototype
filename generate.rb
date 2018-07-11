462fb1a6ded5fe0021d5d34b1bc9ec1c11e29553
Paul Hayes <fofr@users.noreply.github.com>
2018-07-11 16:38:44 +0100

    Prep test for Claydon school

--- generate.rb
+++ generate.rb
@@ -5,7 +5,7 @@ require 'json'
 
 file = File.read('courses-clean.json')
 data = JSON.parse(file)
-provider = ARGV[0] || 'Suffolk and Norfolk Primary SCITT'
+provider = ARGV[0] || 'Claydon High School'
 courses = data.select {|c| c['provider'] == provider }
 
 # if courses.count == 0
